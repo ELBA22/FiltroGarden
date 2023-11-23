d27ba8e0f83ebadca17b7cb4639029ec2e84ffc7
ELBA22 <ELBA22@users.noreply.github.com>
2023-11-23 11:03:40 -0500

    feat: :sparkles: Repositorios

--- Persistence/Configuration/PedidoConfiguration.cs
+++ Persistence/Configuration/PedidoConfiguration.cs
@@ -12,27 +12,27 @@ namespace Persistence.Configuration
     {
         public void Configure(EntityTypeBuilder<Pedido> builder)
         {
-            entity.HasKey(e => e.CodigoPedido).HasName("PRIMARY");
+            builder.HasKey(e => e.CodigoPedido).HasName("PRIMARY");
 
-            entity.ToTable("pedido");
+            builder.ToTable("pedido");
 
-            entity.HasIndex(e => e.CodigoCliente, "FK_pedido_codigo_cliente_cliente_codigo_cliente");
+            builder.HasIndex(e => e.CodigoCliente, "FK_pedido_codigo_cliente_cliente_codigo_cliente");
 
-            entity.Property(e => e.CodigoPedido)
+            builder.Property(e => e.CodigoPedido)
                 .ValueGeneratedNever()
                 .HasColumnName("codigo_pedido");
-            entity.Property(e => e.CodigoCliente).HasColumnName("codigo_cliente");
-            entity.Property(e => e.Comentarios)
+            builder.Property(e => e.CodigoCliente).HasColumnName("codigo_cliente");
+            builder.Property(e => e.Comentarios)
                 .HasColumnType("text")
                 .HasColumnName("comentarios");
-            entity.Property(e => e.Estado)
+            builder.Property(e => e.Estado)
                 .HasMaxLength(15)
                 .HasColumnName("estado");
-            entity.Property(e => e.FechaEntrega).HasColumnName("fecha_entrega");
-            entity.Property(e => e.FechaEsperada).HasColumnName("fecha_esperada");
-            entity.Property(e => e.FechaPedido).HasColumnName("fecha_pedido");
+            builder.Property(e => e.FechaEntrega).HasColumnName("fecha_entrega");
+            builder.Property(e => e.FechaEsperada).HasColumnName("fecha_esperada");
+            builder.Property(e => e.FechaPedido).HasColumnName("fecha_pedido");
 
-            entity.HasOne(d => d.CodigoClienteNavigation).WithMany(p => p.Pedidos)
+            builder.HasOne(d => d.CodigoClienteNavigation).WithMany(p => p.Pedidos)
                 .HasForeignKey(d => d.CodigoCliente)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_pedido_codigo_cliente_cliente_codigo_cliente");
--- Persistence/Data/GardenContext.cs
+++ Persistence/Data/GardenContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
+using System.Reflection;
 
 namespace Persistence.Data;
 
@@ -35,87 +36,10 @@ public partial class GardenContext : DbContext
    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-        modelBuilder.Entity<Cliente>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<DetallePedido>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<Empleado>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<GamaProducto>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<Oficina>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<Pago>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<Pedido>(entity =>
-        {
-
-        });
-
-        modelBuilder.Entity<Producto>(entity =>
-        {
-            entity.HasKey(e => e.CodigoProducto).HasName("PRIMARY");
-
-            entity.ToTable("producto");
-
-            entity.HasIndex(e => e.Gama, "FK_producto_gama_gama_producto_gama");
-
-            entity.Property(e => e.CodigoProducto)
-                .HasMaxLength(15)
-                .HasColumnName("codigo_producto");
-            entity.Property(e => e.CantidadEnStock).HasColumnName("cantidad_en_stock");
-            entity.Property(e => e.Descripcion)
-                .HasColumnType("text")
-                .HasColumnName("descripcion");
-            entity.Property(e => e.Dimensiones)
-                .HasMaxLength(25)
-                .HasColumnName("dimensiones");
-            entity.Property(e => e.Gama)
-                .HasMaxLength(50)
-                .HasColumnName("gama");
-            entity.Property(e => e.Nombre)
-                .HasMaxLength(70)
-                .HasColumnName("nombre");
-            entity.Property(e => e.PrecioProveedor)
-                .HasPrecision(15, 2)
-                .HasColumnName("precio_proveedor");
-            entity.Property(e => e.PrecioVenta)
-                .HasPrecision(15, 2)
-                .HasColumnName("precio_venta");
-            entity.Property(e => e.Proveedor)
-                .HasMaxLength(50)
-                .HasColumnName("proveedor");
-
-            entity.HasOne(d => d.GamaNavigation).WithMany(p => p.Productos)
-                .HasForeignKey(d => d.Gama)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_producto_gama_gama_producto_gama");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
     }
 
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+   
 }
